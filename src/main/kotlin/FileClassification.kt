8b72967c73b2f40fa65e6d176afd12ea530bb63c
Richardial <wolverin309@hotmail.com>
2024-11-13 13:38:01 -0500

    Testing Linux Support (Windows working perfectly)

--- src/main/kotlin/FileClassification.kt
+++ src/main/kotlin/FileClassification.kt
@@ -4,55 +4,65 @@ import kotlin.io.path.Path
 
 class FileClassification {
 
-    companion object{
+    companion object {
         fun redirect(
             file: File, extensions: ArrayList<ExtensionPath>, prefix: ArrayList<Prefix>,
-            black: ArrayList<String>,
-        ){
+            black: ArrayList<String>, os: Boolean
+        ) {
             val patternPref = "^-[A-Z|a-z]".toRegex()
             val hasPrefix = patternPref.find(file.name)?.value
 
-            if(hasPrefix != null){
-                val tempPrefix = file.name.subSequence(0,2)
-                for(pref in prefix){
-                    if(tempPrefix == pref.prefix){
-                        file.renameTo(File(pref.url+ cleanName(file.name,black)))
+            if (hasPrefix != null) {
+                val tempPrefix = file.name.subSequence(0, 2)
+                for (pref in prefix) {
+                    if (tempPrefix == pref.prefix) {
+                        file.renameTo(File(pref.url + cleanName(file.name, black)))
+                        print(file.path)
                     }
                 }
-            }else{
-                redirectByExtension(file, extensions, black)
+            } else {
+                redirectByExtension(file, extensions, black, os)
             }
 
         }
 
-        private fun redirectByExtension(file: File, extensions: ArrayList<ExtensionPath>, black: ArrayList<String>){
-            for(ext in extensions){
-                if(file.extension in ext.extension){
-                    val extraFolder = getFolderInName(file.name,1)
-                    if(extraFolder != null){
-                        Files.createDirectories(Path(ext.url+extraFolder))
-                        file.renameTo(File(ext.url+extraFolder+"\\"+cleanName(file.name, black)))
-                    }else{
-                        file.renameTo(File(ext.url+cleanName(file.name, black)))
+        private fun redirectByExtension(
+            file: File,
+            extensions: ArrayList<ExtensionPath>,
+            black: ArrayList<String>,
+            os: Boolean
+        ) {
+            for (ext in extensions) {
+                if (file.extension in ext.extension) {
+                    val extraFolder = getFolderInName(file.name, 1)
+                    if (extraFolder != null) {
+                        Files.createDirectories(Path(ext.url + extraFolder))
+                        if(os){
+                            file.renameTo(File(ext.url + extraFolder + "\\" + cleanName(file.name, black)))
+                        }else{
+                            file.renameTo(File(ext.url + extraFolder + "/" + cleanName(file.name, black)))
+                        }
+                    } else {
+                        file.renameTo(File(ext.url + cleanName(file.name, black)))
                     }
                 }
             }
         }
 
-        private fun cleanName(name : String, black: ArrayList<String>) : String{
+        private fun cleanName(name: String, black: ArrayList<String>): String {
             var newName = name
-            val fold = getFolderInName(name,0)
+            val fold = getFolderInName(name, 0)
 
-            if(name.startsWith("-")){
+            if (name.startsWith("-")) {
                 newName = newName.drop(3)
             }
 
-            if(fold != null){
-                newName = newName.replace(fold,"")
+            if (fold != null) {
+                newName = newName.replace(fold, "")
             }
 
-            for(b in black){
-                newName = newName.replace(b,"")
+            for (b in black) {
+                newName = newName.replace(b, "")
             }
             return newName
         }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,27 +9,42 @@ fun main() {
     watchDownloads()
 }
 
-fun watchDownloads(){
+fun watchDownloads() {
 
     val json = File("settingsDram.json").readText(Charsets.UTF_8)
     val settings = Json.decodeFromString<Setting>(json)
 
     val url = settings.downloadUrl
-    val downloadFolder  = File(url)
+    val downloadFolder = File(url)
 
     val watchServiceDownloads = FileSystems.getDefault().newWatchService()
     val pathToWatch = downloadFolder.toPath()
     val pathKey = pathToWatch.register(watchServiceDownloads, StandardWatchEventKinds.ENTRY_CREATE)
 
+    var os: Boolean? = null
+
+    if (System.getProperty("os.name").startsWith("Windows")) {
+        os = true
+    } else {
+        os = false
+    }
+
+
+
+
     while (true) {
         val watchKey = watchServiceDownloads.take()
-
+        var file: File? = null
         for (event in watchKey.pollEvents()) {
-            val file = File(url+"\\${event.context()}")
+            if (os) {
+                file = File(url + "\\${event.context()}")
+            } else {
+                file = File(url + "/${event.context()}")
+            }
             Thread.sleep(1000)
-            if(file.extension != "tmp")
+            if (file.extension != "tmp")
                 println(file.path)
-            FileClassification.redirect(file, settings.extensionsPath, settings.prefix, settings.blackList)
+            FileClassification.redirect(file, settings.extensionsPath, settings.prefix, settings.blackList,os)
         }
 
         if (!watchKey.reset()) {
